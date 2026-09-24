c40ad737b0401929c1642ae238d76776628d9535
Kadu Pantoja <dsc.pantoja@gmail.com>
2026-09-23 21:54:22 -0300

    Small Adjustments.

--- avaliacoes/pets/src/main/java/pets/Pet.java
+++ avaliacoes/pets/src/main/java/pets/Pet.java
@@ -1,4 +1,5 @@
 package pets;
+
 /**
  * Classe Pet representa um animal de estimação, que é uma classe abstrata.
  * Ela possui atributos como nome, data de nascimento e uma lista de prontuários.
@@ -12,10 +13,10 @@ import java.util.ArrayList;
 
 public abstract class Pet {
 
-    private String nome; // declara o nome
-    private String nascimento; // declara o nascimento
-    private ArrayList<Prontuario> prontuarios = new ArrayList<>(); // ArrayList contendo a lista de prontuarios de um
-                                                                   // pet
+    private String nome;
+    private String nascimento;
+    private ArrayList<Prontuario> prontuarios = new ArrayList<>();
+
     /**
      * Construtor da classe Pet.
      * Cria uma nova instância de Pet com o nome e data de nascimento fornecidos.
@@ -23,42 +24,47 @@ public abstract class Pet {
      * @param nome
      * @param nascimento
      */
-    public Pet(String nome, String nascimento) { // construtor do PET
+    public Pet(String nome, String nascimento) {
         this.nome = nome;
         this.nascimento = nascimento;
     }
+
     /**
      * Método para obter o nome do pet.
      * 
      * @return o nome do pet.
      */
-    public String getNome() { // Getter do nome
+    public String getNome() {
         return nome;
     }
+
     /**
      * Método para definir o nome do pet.
      * 
      * @param nome o nome a ser definido para o pet.
      */
-    public void setNome(String nome) { // setter do nome
+    public void setNome(String nome) {
         this.nome = nome;
     }
+
     /**
      * Método para obter a data de nascimento do pet.
      * 
      * @return a data de nascimento do pet.
      */
-    public String getNascimento() { // getter do nascimento
+    public String getNascimento() {
         return nascimento;
     }
+
     /**
-     * Método para definir a data de nascimento do pet.	
+     * Método para definir a data de nascimento do pet.
      * 
      * @param nascimento a data de nascimento a ser definida para o pet.
      */
-    public void setNascimento(String nascimento) { // setter do nascimento
+    public void setNascimento(String nascimento) {
         this.nascimento = nascimento;
     }
+
     /**
      * Método para obter a lista de prontuários do pet.
      * 
@@ -67,6 +73,7 @@ public abstract class Pet {
     public ArrayList<Prontuario> getProntuarios() {
         return prontuarios;
     }
+
     /**
      * Método para definir a lista de prontuários do pet.
      * 
@@ -75,12 +82,14 @@ public abstract class Pet {
     public void setProntuarios(ArrayList<Prontuario> prontuarios) {
         this.prontuarios = prontuarios;
     }
+
     /**
      * Método abstrato para obter informações sobre o pet.
-     * Este método deve ser implementado pelas subclasses de Pet para fornecer informações específicas sobre o tipo de animal.
+     * Este método deve ser implementado pelas subclasses de Pet para fornecer
+     * informações específicas sobre o tipo de animal.
      * 
      * @return uma String contendo informações sobre o pet.
      */
-    protected abstract String getInfo(); // getinfo criado para poder ser usado no prontuário
+    protected abstract String getInfo();
 
 }
--- avaliacoes/pets/src/main/java/pets/Tutor.java
+++ avaliacoes/pets/src/main/java/pets/Tutor.java
@@ -1,4 +1,5 @@
 package pets;
+
 /**
  * Classe Tutor representa um tutor de pets, que é uma extensão da classe Pessoa.
  * Ela possui uma lista de pets adotados, métodos para adotar e obter informações sobre a pessoa e os pets.
@@ -7,11 +8,12 @@ package pets;
  * @version 1.0
  */
 import java.util.ArrayList;
-import java.util.List; //importações para o arraylist funcionar
+import java.util.List;
 
-public class Tutor extends Pessoa { // é herdeiro da classe pessoa
+public class Tutor extends Pessoa {
 
-    private List<Pet> pets; // declara a Lista de pets
+    private List<Pet> pets;
+
     /**
      * Construtor da classe Tutor.
      * Cria uma nova instância de Tutor com o nome e apelido fornecidos.
@@ -20,42 +22,46 @@ public class Tutor extends Pessoa { // é herdeiro da classe pessoa
      * @param nome
      * @param apelido
      */
-    public Tutor(String nome, String apelido) { // construtor da classe tutor
+    public Tutor(String nome, String apelido) {
         super(nome, apelido);
         this.pets = new ArrayList<>();
     }
+
     /**
      * Método para adotar um pet.
      * Adiciona o pet à lista de pets do tutor.
      * 
      * @param pet o pet a ser adotado pelo tutor.
      */
-    public void adotar(Pet pet) { // método para adotar o pet, adcionando o pet á lista
-        pets.add(pet); // do tutor. Funciona como o setter da Lista pets
+    public void adotar(Pet pet) {
+        pets.add(pet);
     }
+
     /**
      * Método para obter a lista de pets adotados pelo tutor.
      * 
      * @return a lista de pets adotados.
      */
-    public List<Pet> getPets() { // Getter da lista pets
+    public List<Pet> getPets() {
         return pets;
     }
+
     /**
      * Método para criar uma String com as informações do tutor e seus pets.
-     * Sobrescreve o método getInfo da classe Pessoa para incluir informações sobre os pets adotados.
+     * Sobrescreve o método getInfo da classe Pessoa para incluir informações sobre
+     * os pets adotados.
      * 
-     * @return uma String contendo o nome, apelido e informações dos pets adotados pelo tutor.
+     * @return uma String contendo o nome, apelido e informações dos pets adotados
+     *         pelo tutor.
      */
-    @Override // dá um override para sobrecarregar o getInfo da pessoa
+    @Override
     public String getInfo() {
-        String info = super.getInfo() + "\nPets: "; // armazena as informacoes numa variavel info, para depois imprimir
-                                                    // // ela
-        for (Pet pet : pets) { // um loop para armazenar todos os pets que o tutor venha a ter, para imprimir
-                               // depois
+        String info = super.getInfo() + "\nPets: ";
+
+        for (Pet pet : pets) {
             info += pet.getInfo() + "; ";
         }
-        return info; // imprime a variavel info 
+        return info;
     }
 
 }
